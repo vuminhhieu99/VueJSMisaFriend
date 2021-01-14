df978207c86d6365a384cb6a4b8af1ea2b0e624d
vuhieupro1999@gmail.com <vuhieupro1999@gmail.com>
2021-01-14 13:52:25 +0700

    initiative responsitory

---
+++ Project/src/components/User.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="component">
+        <p>Ten toi la {{ name }}</p>
+        <p>Toi nam nay {{ age }} tuoi</p>
+        <hr>
+        <div class="row">
+            <div class="col-xs-12 col-sm-6 col-6">
+                <app-user-detail></app-user-detail>
+            </div>
+            <div class="col-xs-12 col-sm-6 col-6">
+                <app-user-edit></app-user-edit>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import UserDetail from './UserDetail.vue';
+    import UserEdit from './UserEdit.vue';
+
+    export default {
+        data: function () {
+            return {
+                name: 'Duy',
+                age: 23
+            };
+        },
+
+        components: {
+            appUserDetail: UserDetail,
+            appUserEdit: UserEdit
+        }
+    }
+</script>
---
+++ Project/src/components/UserEdit.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="component">
+        <h3>Edit User</h3>
+        <p>Tuoi cua toi: </p>
+        <button>Thay doi</button>
+    </div>
+</template>
+
+<script>
+</script>
